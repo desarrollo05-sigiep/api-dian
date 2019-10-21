d68425e112dce12dfb3881d6f8ac81cc45477368
German <german191215@hotmail.com>
2019-10-20 22:20:53 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,43 +22,39 @@ Route::prefix('/ubl2.1')->group(function () {
 });
 
 Route::middleware('auth:api')->group(function () {
-    // UBL 2.1
-    Route::prefix('/ubl2.1')->group(function () {
-        // Configuration
-        Route::prefix('/config')->group(function () {
-            Route::put('/software', 'Api\ConfigurationController@storeSoftware');
-            Route::put('/certificate', 'Api\ConfigurationController@storeCertificate');
-            Route::put('/resolution', 'Api\ConfigurationController@storeResolution');
-            Route::put('/environment', 'Api\ConfigurationController@storeEnvironment');
-        });
+    Route::group(['middleware' => 'cors'], function () {
+        Route::prefix('/ubl2.1')->group(function () {
+            // Configuration
+            Route::prefix('/config')->group(function () {
+                Route::put('/software', 'Api\ConfigurationController@storeSoftware');
+                Route::put('/certificate', 'Api\ConfigurationController@storeCertificate');
+                Route::put('/resolution', 'Api\ConfigurationController@storeResolution');
+                Route::put('/environment', 'Api\ConfigurationController@storeEnvironment');
+            });
 
-        // Invoice
-        Route::prefix('/invoice')->group(function () {
-            Route::post('/{testSetId}', 'Api\InvoiceController@testSetStore');
-            Route::post('/', 'Api\InvoiceController@store');
-        });
+            // Invoice
+            Route::prefix('/invoice')->group(function () {
+                Route::post('/{testSetId}', 'Api\InvoiceController@testSetStore');
+                Route::post('/', 'Api\InvoiceController@store');
+            });
 
-        // Credit Notes
-        Route::prefix('/credit-note')->group(function () {
-            Route::post('/{testSetId}', 'Api\CreditNoteController@testSetStore');
-            Route::post('/', 'Api\CreditNoteController@store');
-        });
+            // Credit Notes
+            Route::prefix('/credit-note')->group(function () {
+                Route::post('/{testSetId}', 'Api\CreditNoteController@testSetStore');
+                Route::post('/', 'Api\CreditNoteController@store');
+            });
 
-        // Debit Notes
-        Route::prefix('/debit-note')->group(function () {
-            Route::post('/{testSetId}', 'Api\DebitNoteController@testSetStore');
-            Route::post('/', 'Api\DebitNoteController@store');
-        });
+            // Debit Notes
+            Route::prefix('/debit-note')->group(function () {
+                Route::post('/{testSetId}', 'Api\DebitNoteController@testSetStore');
+                Route::post('/', 'Api\DebitNoteController@store');
+            });
 
-        // Status
-        Route::prefix('/status')->group(function () {
-            Route::post('/zip/{trackId}', 'Api\StateController@zip');
-            Route::post('/document/{trackId}', 'Api\StateController@document');
+            // Status
+            Route::prefix('/status')->group(function () {
+                Route::post('/zip/{trackId}', 'Api\StateController@zip');
+                Route::post('/document/{trackId}', 'Api\StateController@document');
+            });
         });
     });
-});
-
-Route::group(['middleware' => 'cors'], function () {
-    Route::get('getData', 'v1\MyController@getData');
-    Route::get('getData2', 'v1\MyController@getData2');
 });
